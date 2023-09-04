1765dfd3000f0fd09a359b94d71f0d50bc186cdb
PofuXD <138972462+PofuXD@users.noreply.github.com>
2023-09-04 17:56:42 +0300

    The necessary items have been created, the game menu has been created, and the StartGame method has been created.

---
+++ GameManager/GameManage.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TextBasedGame.GameManager.GameCycle;
+
+namespace TextBasedGame.GameManager
+{
+    internal class GameManage
+    {
+        int choose;
+        Player player = new Player();
+        public void StartGame()
+        {
+            Gameİnit();
+        }
+
+        public void Gameİnit()
+        {
+            Console.WriteLine("Enter a name: ");
+            player.Name = Console.ReadLine();
+            while (true)
+            {
+                Console.WriteLine("Choose a weapon: ");
+                Console.WriteLine("1-LongSword");
+                Console.WriteLine("2-Dagger");
+                Console.WriteLine("3-Staff");
+                try
+                {
+                    choose = Convert.ToInt32(Console.ReadLine());
+                    switch (choose)
+                    {
+                        case 1:
+                            player = new LongSword();
+                            break;
+                        case 2:
+                            player = new Dagger();
+                            break;
+                        case 3:
+                            player = new Staff();
+                            break;
+                        default:
+                            Console.WriteLine("Please choose a valid option: ");
+                            break;
+                    }
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Please choose a valid option: ");
+                    Thread.Sleep(500);
+                    Console.Clear();
+                }
+            }
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,15 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using TextBasedGame.GameManager;
 
 namespace TextBasedGame
 {
     internal class Program
     {
+        static GameManage manage = new GameManage();
+
         static void Main(string[] args)
         {
+            int x = 0;
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Welcome the Ant-Maze");
+                Console.WriteLine("1-Start Game");
+                Console.WriteLine("2-Load Game");
+                Console.WriteLine("3-Exit");
+                try { x = Convert.ToInt32(Console.ReadLine()); } catch { Console.WriteLine("Please choose a valid option: "); Thread.Sleep(500); }
+                switch (x)
+                {
+                    case 1:
+                        manage.StartGame();
+                        break;
+                    case 2:
 
-            Console.ReadKey();
+                        break;
+                    case 3:
+                        Environment.Exit(0);
+                        break;
+                }
+            }
         }
     }
 }
